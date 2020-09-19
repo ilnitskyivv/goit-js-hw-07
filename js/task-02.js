6001de4bd028ec880800c27f710d1dc958f03037
Volodymyr Ilnitskyi <ilnitskyiv2@gmail.com>
2020-09-19 23:06:30 +0300

    Решает первые три задания

---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const ingredientsNew = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  return itemEl;
+});
+
+const ingredientsEl = document.querySelector('#ingredients');
+ingredientsEl.append(...ingredientsNew);
